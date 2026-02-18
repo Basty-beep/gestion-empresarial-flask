292edc7553c4ed85f79883c5935fcd3f8fbca132
Bastian Guajardo <bastian.guajardo.baeza@gmail.com>
2026-02-18 12:35:26 -0300

    app.py remodelado y formularios implementados para agregar productos

--- app.py
+++ app.py
@@ -1,24 +1,40 @@
 from modulos.inventario import cargar_inventario, guardar_inventario
 
-# inventario de prueba
-inventario_prueba = {
-    "lapiz": {"precio": 100, "cantidad": 10, "categoria": "utiles"},
-    "cuaderno": {"precio": 1500, "cantidad": 5, "categoria": "utiles"}
-}
-
-# guardar datos
-guardar_inventario(inventario_prueba)
-
-# cargar datos
-datos = cargar_inventario()
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 @app.route('/')
 @app.route('/inventario')
 def index():
-    return render_template('inventario/lista.html', datos = datos)
+    datos = cargar_inventario()
+    return render_template('inventario/lista.html', datos=datos)
+
+# Agregar productos
+
+@app.route('/inventario/agregar', methods = ['GET', 'POST'])
+def agregar():
+    if request.method == 'POST':
+        producto = request.form['producto']
+        precio = int(request.form['precio'])
+        cantidad = int(request.form['cantidad'])
+        categoria = request.form['categoria']
+
+        # cargar inventario actual
+        inventario = cargar_inventario()
+
+        # agregar producto nuevo
+        inventario[producto] = {
+            "precio": precio,
+            "cantidad": cantidad,
+            "categoria": categoria
+        }
+
+        # guardar en el archivo json
+        guardar_inventario(inventario)
+
+        # volver a la lista
+        return redirect(url_for('index'))
+    return render_template('inventario/agregar.html')
 
 
 
