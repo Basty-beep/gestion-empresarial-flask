8ec182b3cfad34085e46646a3a9a8bcf5dc19595
Bastian Guajardo <bastian.guajardo.baeza@gmail.com>
2026-02-24 22:19:53 -0300

    Se agrega función para crear carpeta

--- modulos/inventario.py
+++ modulos/inventario.py
@@ -1,12 +1,14 @@
-import json
+import json, os
 
 def cargar_inventario(archivo="datos/inventario.json"):
-    try:
-        with open(archivo, "r", encoding="utf-8") as f:
-            return json.load(f)
-    except FileNotFoundError:
+    if not os.path.exists(archivo):
         return {}
 
+    with open(archivo, "r", encoding="utf-8") as f:
+        return json.load(f)
+
 def guardar_inventario(inventario, archivo="datos/inventario.json"):
+    os.makedirs(os.path.dirname(archivo), exist_ok=True)
+
     with open(archivo, "w", encoding="utf-8") as f:
         json.dump(inventario, f, indent=4, ensure_ascii=False)
